2878830aa6fa87c980a4fa407588668b6cdee46c
Catherine Jones <cath@bitgirl101.com>
2016-09-12 18:55:16 +1000

    hopefully fix missing css in prod

--- config.rb
+++ config.rb
@@ -1,1 +1,11 @@
 set :css_dir, "stylesheets"
+
+# Global site settings (not shown here)
+set :site_url, ""
+# ...
+
+configure :build do
+  # Relative assets needed to deploy to Github Pages
+  activate :relative_assets
+  set :site_url, "/repo-name"
+end
